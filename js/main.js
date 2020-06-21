f63a0f0a79f21e439dbf9a23a378b90023265a20
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-06-21 13:42:00 +0300

    сделал 9 урок

---
+++ js/main.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const calculate = document.getElementById('start');
+
+console.dir(calculate);
+
+const data = document.querySelector('.data');
+
+const buttonPlus = data.getElementsByTagName('button');
+
+console.log(buttonPlus);
+
+const checkbox = document.querySelector('#deposit-check');
+
+console.log(checkbox);
+
+const incomeItem = document.querySelectorAll('additional_income-item');
+
+console.log(incomeItem);
+
+const result = document.querySelector('.result');
+
+console.log(result);
+
+const budgetMonth = result.getElementsByClassName('budget_month-value')[0];
+
+console.log(budgetMonth);
+
+const budgetDay = result.getElementsByClassName('budget_day-value')[0];
+
+console.log(budgetDay);
+
+const expensesMonth = result.getElementsByClassName('expenses_month-value')[0];
+
+console.log(expensesMonth);
+
+const additionalIncome = result.getElementsByClassName('additional_income-value')[0];
+
+console.log(additionalIncome);
+
+const additionalExpenses = result.getElementsByClassName('additional_expenses-value')[0];
+
+console.log(additionalExpenses);
+
+const incomePeriod = result.getElementsByClassName('income_period-value')[0];
+
+console.log(incomePeriod);
+
+const targetMonth = result.getElementsByClassName('target_month-value')[0];
+
+console.log(targetMonth);
+
+const salaryAmount = document.querySelector('.salary-amount');
+
+console.log(salaryAmount);
+
+const incomeTitles = document.querySelector('.income-items');
+
+const incomeTitle = incomeTitles.querySelector('.income-title');
+
+console.log(incomeTitle);
+
+const incomeAmount = document.querySelector('.income-amount');
+
+console.log(incomeAmount);
+
+const additionalIncomeData1 = document.querySelectorAll('.additional_income-item')[0];
+
+console.log(additionalIncomeData1);
+
+const additionalIncomeData2 = document.querySelectorAll('.additional_income-item')[1];
+
+console.log(additionalIncomeData2);
+
+const expensesItems = document.querySelector('.expenses-items');
+
+const expensesTitle = expensesItems.querySelector('.expenses-title');
+
+console.log(expensesTitle);
+
+const expensesAmount = document.querySelector('.expenses-amount');
+
+console.log(expensesAmount);
+
+const depositAmount = document.querySelector('.deposit-amount');
+
+console.log(depositAmount);
+
+const depositPercent = document.querySelector('.deposit-percent');
+
+console.log(depositPercent);
+
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+
+console.log(additionalExpensesItem);
+
+const targetAmount = document.querySelector('.target-amount');
+
+console.log(targetAmount);
+
+const periodSelect = document.querySelector('.period-select');
+
+console.log(periodSelect);
+
+
+
+
+
+
+
+
+
+
+
